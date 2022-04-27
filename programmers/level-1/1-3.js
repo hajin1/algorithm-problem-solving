89cbb7ffdae0b07d7f3bf633a6366ccdc54a429c
hajin <hajinkim@kangwon.ac.kr>
2022-04-27 21:47:02 +0900

    내적

---
+++ programmers/level-1/1-3.js
@@ -0,0 +1,15 @@
+/**
+ * 내적
+ * https://programmers.co.kr/learn/courses/30/lessons/70128 
+ * 2022-04-27
+ */
+
+function solution(a, b) {
+    let answer = 0;
+
+    for (let i = 0; i < a.length; i++) {
+        answer += a[i] * b[i];
+    }
+
+    return answer;
+}
